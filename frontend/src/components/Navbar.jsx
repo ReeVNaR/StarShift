f4f3c8eaf9281c40e72c7c48a065db13b466d2e3
Sahil Ambekar <sahil932004ssa@gmail.com>
2025-03-20 12:44:52 +0530

    logo added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <Link to="/" className={`font-bold text-xl ${theme.text}`}>
-              StarShift
+              <img src="STARSHIFT LOGO 1.svg" alt="" className='h-10 w-auto' />
             </Link>
             <div className="flex items-center space-x-4 text-gray-300">
               <Link to="/products" className="hover:text-blue-400">Products</Link>
